4b757a79ab1f66d59a001fabb05897cababb1008
Palak Sharma <sharmapalak7676@gmail.com>
2026-02-04 22:52:05 +0530

    Create intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,27 @@
+//private vs public access control 
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int marks;   
+
+public:
+    void setMarks(int m) {
+        marks = m;
+    }
+
+    void showMarks() {
+        cout << "Marks: " << marks;
+    }
+};
+
+int main() {
+    Student s;
+
+    
+    s.setMarks(90);    
+    s.showMarks();      
+
+    return 0;
+}
